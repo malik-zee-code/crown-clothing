f3339415f5cced3c15cf41728c24af203ff72f8a
Zeeshan Haider <ze3063786@gmail.com>
2021-05-01 18:14:28 +0500

    fixing

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
-import {persistStore} from "redux-persist"
+import { persistStore } from "redux-persist";
 
-const middleWare =[logger]
+const middleWare = [logger];
 
-export const store = createStore(rootReducer,applyMiddleware(...middleWare))
-export const persistor = persistStore(store)
-
-export default {store,persistor};
+export const store = createStore(rootReducer, applyMiddleware(middleWare));
+export const persistor = persistStore(store);
